e225413e164cf68bfd957c7c5fc65ddc265e1a2f
Neil Northrop <nnorthrop@gmail.com>
2015-04-03 00:05:23 -0700

    Inventory test cleanup

--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -11,12 +11,15 @@ class InventoryTest < MiniTest::Unit::TestCase
 
   def test_add_item
     @inventory.add(@item)
+
     assert_equal @item, @inventory.items.last
     assert_equal 1, @inventory.strength
+
 
     second_item = Item.random
     expected_strength = @inventory.strength + second_item.strength
     @inventory.add(second_item)
+
     assert_equal second_item, @inventory.items.last
     assert_equal expected_strength, @inventory.strength
   end
@@ -28,6 +31,10 @@ class InventoryTest < MiniTest::Unit::TestCase
 
   def test_search_items
     @inventory.add(@item)
+    random_item = Item.random
+    @inventory.add(random_item)
+
+    assert_equal random_item, @inventory.search_items(random_item.name)
     assert_equal @item, @inventory.search_items(@item.name)
   end
 
