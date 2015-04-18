19262436b73474c5316665bc053897735ab21978
Neil Northrop <nnorthrop@gmail.com>
2015-04-17 20:47:18 -0700

    add strength attr to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,20 @@
 require_relative 'inventory'
 
 class Player
-  attr_accessor :inventory
+  attr_accessor :inventory, :strength
   attr_reader :position
 
   def initialize
-    @position = "0|0"
+    @position  = "0|0"
     @inventory = Inventory.new
   end
 
   def update_position(new_position)
     @position = new_position
+  end
+
+  def update_strength
+    self.strength = inventory.strength
   end
 
 end
