b08917df6202421b4c7aa107170aa03b2722985d
Neil Northrop <nnorthrop@gmail.com>
2015-05-13 07:27:10 -0700

    clean up test strings

--- test/dispatch_test.rb
+++ test/dispatch_test.rb
@@ -8,61 +8,61 @@ class DispatchTest < MiniTest::Unit::TestCase
 
   def setup
     @dispatch      = Dispatch.new
-    @expected_item = Item.new({"name" => "test", "strength" => 1})
-    @second_item   = Item.new({"name" => "cat", "strength" => 2})
-    @help_string   = "list of words you can use: move, inventory, attack, pickup, drop, and dance!"
+    @expected_item = Item.new({'name' => 'test', 'strength' => 1})
+    @second_item   = Item.new({'name' => 'cat', 'strength' => 2})
+    @help_string   = 'list of words you can use: move, inventory, attack, pickup, drop, and dance!'
   end
 
   def test_execute_command
-    assert_equal "That's not a command!", @dispatch.execute("lakjsd;lkfjks")
+    assert_equal "That's not a command!", @dispatch.execute('lakjsd;lkfjks')
   end
 
   def test_responds_to
-    assert_respond_to @dispatch, "help"
-    assert_respond_to @dispatch, "burn"
-    assert_respond_to @dispatch, "pickup"
+    assert_respond_to @dispatch, 'help'
+    assert_respond_to @dispatch, 'burn'
+    assert_respond_to @dispatch, 'pickup'
   end
 
   def test_get_inventory_with_single_item
     @dispatch.inventory.items << @expected_item
 
-    assert_equal "You have: test with a strength of 1", @dispatch.execute("inventory")
+    assert_equal 'You have: test with a strength of 1', @dispatch.execute('inventory')
   end
 
   def test_get_inventory_with_multiple_items
-    expected_string = "You have: test with a strength of 1 and cat with a strength of 2"
+    expected_string = 'You have: test with a strength of 1 and cat with a strength of 2'
     @dispatch.inventory.items << @expected_item << @second_item
 
     assert_equal expected_string, @dispatch.get_inventory
   end
 
   def test_list
-    expected_string = "me, you, and I"
-    actual_array    = ["me", "you", "I"]
+    expected_string = 'me, you, and I'
+    actual_array    = ['me', 'you', 'I']
 
     assert_equal expected_string, @dispatch.list(actual_array)
   end
 
   def test_prefix
-    assert_equal "a dog", @dispatch.prefix("dog")
-    assert_equal "an axe", @dispatch.prefix("axe")
+    assert_equal 'a dog', @dispatch.prefix('dog')
+    assert_equal 'an axe', @dispatch.prefix('axe')
   end
 
   def test_pickup
-    expected_string = "Stick picked up!"
+    expected_string = 'Stick picked up!'
 
-    assert_equal expected_string, @dispatch.pickup("stick")
+    assert_equal expected_string, @dispatch.pickup('stick')
 
-    expected_string = "Could not find a tick!"
+    expected_string = 'Could not find a tick!'
     @dispatch.map = World.new
 
-    assert_equal expected_string, @dispatch.pickup("tick")
+    assert_equal expected_string, @dispatch.pickup('tick')
 
-    expected_string = "Nothing to pick up!"
-    zone = Zone.new("Test Zone")
-    @dispatch.map.world["0|0"] = zone
+    expected_string = 'Nothing to pick up!'
+    zone = Zone.new('Test Zone')
+    @dispatch.map.world['0|0'] = zone
 
-    assert_equal expected_string, @dispatch.pickup("something")
+    assert_equal expected_string, @dispatch.pickup('something')
   end
 
 end
